6442170f4734d99bb3917b2fa664d3cdd9dfe2f1
Thomas <tzhao2@stuy.edu>
2018-02-10 20:58:56 -0500

    Seems like addQueen and removeQueen are working as intended

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -20,20 +20,46 @@ public class QueenBoard{
     private boolean addQueen(int row, int col){
 	if(board[row][col] == 0){
 	    board[row][col] = board[row][col] - 1;
-	    for(int colIndex = 0; colIndex < board[row].length; colIndex++){
-		if(colIndex != col){
-		    board[row][colIndex] = board[row][colIndex] + 1;
-		}
-	    }
-	    //need to make vertical and diagonal threatened spaces
+	    addRemoveHelp(row, col, 0, 1, 1);
+	    addRemoveHelp(row, col, 0, -1, 1);
+	    addRemoveHelp(row, col, -1, 0, 1);
+	    addRemoveHelp(row, col, 1, 0, 1);
+	    addRemoveHelp(row, col, 1, 1, 1);
+	    addRemoveHelp(row, col, 1, -1, 1);
+	    addRemoveHelp(row, col, -1, 1, 1);
+	    addRemoveHelp(row, col, -1, -1, 1);
 	    return true;
 	}else{
 	    return false;
 	}
     }
 
-	//privtae boolean removeQueen(int row, int col){
-	//}
+    public void addRemoveHelp(int row, int col, int rowInc, int colInc, int addOrRemove){
+	for(int colIndex = col + colInc, rowIndex = row + rowInc;
+		colIndex < board[row].length && rowIndex < board.length && colIndex >= 0 && rowIndex >= 0;
+		colIndex = colIndex + colInc, rowIndex = rowIndex + rowInc){
+		board[rowIndex][colIndex] = board[rowIndex][colIndex] + addOrRemove;
+	    }
+    }
+
+    private boolean removeQueen(int row, int col){
+	if(board[row][col] == -1){
+	    board[row][col] = board[row][col] + 1;
+	    addRemoveHelp(row, col, 0, 1, -1);
+	    addRemoveHelp(row, col, 0, -1, -1);
+	    addRemoveHelp(row, col, -1, 0, -1);
+	    addRemoveHelp(row, col, 1, 0, -1);
+	    addRemoveHelp(row, col, 1, 1, -1);
+	    addRemoveHelp(row, col, 1, -1, -1);
+	    addRemoveHelp(row, col, -1, 1, -1);
+	    addRemoveHelp(row, col, -1, -1, -1);
+	    return true;
+	}else{
+	    return false;
+	}
+    }
+
+
 
 
 
@@ -52,11 +78,35 @@ public class QueenBoard{
     
     public static void main(String[] args){
 	QueenBoard a = new QueenBoard(5);
+
+	System.out.println("-----addQueen-----");
 	
 	System.out.println(a);
 
-	a.addQueen(2,2);
+	System.out.println(a.addQueen(0,0));
+	System.out.println(a);
 
+	System.out.println(a.addQueen(0,0));
+	System.out.println(a);
+
+	System.out.println(a.addQueen(4,4));
+	System.out.println(a);
+
+	System.out.println(a.addQueen(3,4));
+	System.out.println(a);
+
+	System.out.println("-----removeQueen-----");
+
+	System.out.println(a.removeQueen(3,4));
+	System.out.println(a);
+
+	System.out.println(a.removeQueen(0,2));
+	System.out.println(a);
+
+	System.out.println(a.removeQueen(0,4));
+	System.out.println(a);
+
+	System.out.println(a.removeQueen(0,0));
 	System.out.println(a);
     }
 }
