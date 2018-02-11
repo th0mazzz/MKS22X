b80b3266a6ac3cc7b1525c69fbdcdb6675de6fbe
Thomas <tzhao2@stuy.edu>
2018-02-10 23:51:53 -0500

    Partially got solve and solveHelp working, they work for one runthough

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -10,7 +10,12 @@ public class QueenBoard{
 	String returnString = "";
 	for(int rowIndex = 0; rowIndex < board.length; rowIndex++){
 	    for(int colIndex = 0; colIndex < board[rowIndex].length; colIndex++){
-		returnString = returnString + board[rowIndex][colIndex] + " ";
+		if(board[rowIndex][colIndex] == -1){
+		    returnString = returnString + "Q ";
+		}
+		else{
+		    returnString = returnString + board[rowIndex][colIndex] + " ";
+		}
 	    }
 	    returnString = returnString + "\n";
 	}
@@ -34,14 +39,6 @@ public class QueenBoard{
 	}
     }
 
-    public void addRemoveHelp(int row, int col, int rowInc, int colInc, int addOrRemove){
-	for(int colIndex = col + colInc, rowIndex = row + rowInc;
-		colIndex < board[row].length && rowIndex < board.length && colIndex >= 0 && rowIndex >= 0;
-		colIndex = colIndex + colInc, rowIndex = rowIndex + rowInc){
-		board[rowIndex][colIndex] = board[rowIndex][colIndex] + addOrRemove;
-	    }
-    }
-
     private boolean removeQueen(int row, int col){
 	if(board[row][col] == -1){
 	    board[row][col] = board[row][col] + 1;
@@ -59,10 +56,52 @@ public class QueenBoard{
 	}
     }
 
+    public void addRemoveHelp(int row, int col, int rowInc, int colInc, int addOrRemove){
+	for(int colIndex = col + colInc, rowIndex = row + rowInc;
+		colIndex < board[row].length && rowIndex < board.length && colIndex >= 0 && rowIndex >= 0;
+		colIndex = colIndex + colInc, rowIndex = rowIndex + rowInc){
+		board[rowIndex][colIndex] = board[rowIndex][colIndex] + addOrRemove;
+	    }
+    }
+
+    public boolean solve(){
+	for(int rowIndex = 0; rowIndex < board.length; rowIndex++){
+	    for(int colIndex = 0; colIndex < board[rowIndex].length; colIndex++){
+		if(board[rowIndex][colIndex] != 0){
+		    throw new IllegalStateException();
+		}
+	    }
+	}
+	return solveHelp(0, 0, board.length, 0);
+    }
+
+    public boolean solveHelp(int row, int col, int n, int numQueens){
+	System.out.println("Row, col, numQueens :" + row + ", " + col + ", " + numQueens);
+	System.out.println(this);
+
+	for(; col < board[row].length; col++){
+	    int rowIndex = row;
+	    int colIndex = col;
+	    for(; rowIndex < board.length; rowIndex++){
+		if(addQueen(rowIndex, colIndex)){
+		    numQueens++;
+		    while(rowIndex < board.length){
+			return solveHelp(0, 0, n, numQueens);
+			
+		    }
+		}
+	    }
+	}
 
 
 
 
+	
+	if(numQueens == n){
+	    return true;
+	}
+	return false;
+    }
 
 
     
@@ -108,5 +147,18 @@ public class QueenBoard{
 
 	System.out.println(a.removeQueen(0,0));
 	System.out.println(a);
+
+	System.out.println("-----solve-----");
+
+	System.out.println(a.solve());
+	System.out.println(a);
+
+	//a.board[2][2] = 2;
+	//System.out.println(a);
+
+	//System.out.println(a.solve());
+	//System.out.println(a);
+
+	
     }
 }
