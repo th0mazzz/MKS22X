c2ae7b1b2e5c64ed1d27e1b13edfcc960fce6abe
Thomas <tzhao2@stuy.edu>
2018-02-16 02:46:35 -0500

    Lab day, started working on solveH, was gonna edit toString

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -2,7 +2,7 @@ public class KnightBoard{
 
     int[][] board;
 
-    int[] movesRow = {2,  2, 1, -1, -2  -2,  1, -1};
+    int[] movesRow = {2,  2, 1, -1, -2, -2,  1, -1};
     int[] movesCol = {1, -1, 2,  2,  1, -1, -2, -2};
     
 
@@ -24,7 +24,13 @@ public class KnightBoard{
 	String returnString = "";
 	for(int row = 0; row < board.length; row++){
 	    for(int col = 0; col < board[row].length; col++){
-		returnString += board[row][col] + " ";
+		//if(board.length * board[row].length < 10){
+		    returnString += board[row][col] + " ";
+		    //}
+		    //else{
+		    //if(
+		    //returnString += board[row][col] + " ";
+		    //}
 	    }
 	    returnString += "\n";
 	}
@@ -42,21 +48,46 @@ public class KnightBoard{
 		}
 	    }
 	}
-
-	return solveH(startingRow, startingCol);
+	return solveH(startingRow, startingCol, 1);
     }
     
+    /*
+    //this was for testing if knight move nums are right
     private boolean solveH(int row, int col){
+	board[row][col] = 5;
+	for(int i = 0; i < movesRow.length; i++){
+	    System.out.println(this);
+	    board[row + movesRow[i]][col + movesCol[i]] = 9;
+	}
 	return true;
     }
+    */
 
-
-
-
+    private boolean solveH(int row, int col, int level){
+	try{
+	    System.out.println(level + "\n" + this);
+	    if(board[row][col] == 0){
+		board[row][col] = level;
+		level++;
+	    }
+	    for(int loop = 0; loop < movesRow.length; loop++){
+		if(solveH(row + movesRow[loop], col + movesCol[loop], level++)){
+		    return true;
+		}
+		//board[row][col] = 0;
+	    }
+	    return true;
+	}catch(Exception ArrayIndexOutOfBoundsException){
+	    return false;
+	}
+    }
+    
     public static void main(String[] args){
 
-	KnightBoard a = new KnightBoard(5, 5);
+	KnightBoard a = new KnightBoard(7, 7);
 
+	//System.out.println(a);
+	//a.solve(3, 3);
 	System.out.println(a);
 	
 	
