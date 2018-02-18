980f9a71c37ed09cd308b53c15caad7f1f10f09c
Thomas <tzhao2@stuy.edu>
2018-02-17 21:59:06 -0500

    Nothing still works: my solve only works for certain configurations on boards, my countSolutions doesn't work either! UGHHHHHHHHHHHHH

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -6,7 +6,7 @@ public class KnightBoard{
     int[] movesCol = {1, -1, 2,  2,  1, -1, -2, -2};
 
     String[] debug = {"2 Down 1 Right", "2 Down 1 Left", "2 Right 1 Down", "2 Right 1 Up",
-		    "2 Up 1 Right", "2 Up 1 Left", "2 Left 1 Down", "2 Left 1 Up"};
+		      "2 Up 1 Right", "2 Up 1 Left", "2 Left 1 Down", "2 Left 1 Up"};
     
 
     // Is zero included?
@@ -67,79 +67,19 @@ public class KnightBoard{
 	}
 	return solveH(startingRow, startingCol, 1);
     }
-    
-    /*
-    //this was for testing if knight move nums are right
-    private boolean solveH(int row, int col){
-	board[row][col] = 5;
-	for(int i = 0; i < movesRow.length; i++){
-	    System.out.println(this);
-	    board[row + movesRow[i]][col + movesCol[i]] = 9;
-	}
-	return true;
-    }
-    */
-
-    /*
-    private boolean solveH(int row, int col, int level){
-	try{
-	    System.out.println("Level: " + level);
-	    System.out.println(level - 1);
-	    //System.out.println(Text.go(1,1));
-	    System.out.println("\n" + this);
-	    //Text.wait(2); //adjust this delay
-	    //System.out.println("Looking at: " + "(" + row + ", " + col + ")   Equal to 0?: ");
-	    //System.out.println("Boolean: ");
-	    System.out.println(board[row][col] == 0);
-	    if(board[row][col] == 0){
-		//System.out.println("Replacing with " + level);
-		board[row][col] = level;
-		level++;
-	    }else{
-		return false;
-	    }
-	    
-	    for(int loop = 0; loop < movesRow.length; loop++){
-		//System.out.println(debug[loop] + ", Loop: " + loop);
-		//System.out.println("Numbers: " + movesRow[loop] + ", " + movesCol[loop]);
-		//System.out.println("Current location: " + row + ", " + col);		
-		if(solveH(row + movesRow[loop], col + movesCol[loop], level)){
-		    return true;
-		}
-		//board[row][col] = 0;
-	    }
-
-	    //System.out.println(level - 1 == board.length * board[row].length);
-	    if(level - 1 == board.length * board[row].length){
-		//System.out.println("Here!");
-	    	return true;
-	    }
-	    
-	    //System.out.println("Made it to the false (non-exception)");
-	    board[row][col] = 0;
-	    return false;
-	}catch(Exception ArrayIndexOutOfBoundsException){
-	    //System.out.println("Caught exception");
-	    return false;
-	}
-    }
-    */
 
     public boolean solveH(int row, int col, int level){
 	/*
-	System.out.println(Text.go(1,1));
-	System.out.println("\n" + this);
-	System.out.println("Level: " + level);
-	Text.wait(0); //adjust this delay
+	  System.out.println(Text.go(1,1));
+	  System.out.println("\n" + this);
+	  System.out.println("Level: " + level);
+	  Text.wait(0); //adjust this delay
 	*/
 
 	if(row < 0 || row >= board.length || col < 0 || col >= board[row].length){
 	    return false;
 	}
-	
-	
-	
-	
+
 	if(board[row][col] == 0){
 	    board[row][col] = level;
 	    level++;
@@ -162,6 +102,56 @@ public class KnightBoard{
 	board[row][col] = 0;
 	return false;
     }
+
+    public int countSolutions(int startingRow, int startingCol){
+	if(startingRow < 0 || startingCol < 0){
+	    throw new IllegalStateException();
+	}
+	for(int row = 0; row < board.length; row++){
+	    for(int col = 0; col < board[row].length; col++){
+		if(board[row][col] != 0){
+		    throw new IllegalStateException();
+		}
+	    }
+	}
+	return countH(startingRow, startingCol, 1);
+    }
+
+    public int countH(int row, int col, int level){
+
+	/*
+	  System.out.println(Text.go(1,1));
+	  System.out.println("\n" + this);
+	  System.out.println("Level: " + level);
+	  Text.wait(0); //adjust this delay
+	*/
+	int numSolutions = 0;
+	
+	if(row < 0 || row >= board.length || col < 0 || col >= board[row].length){
+	    return 0;
+	}
+
+	if(board[row][col] == 0){
+	    board[row][col] = level;
+	    level++;
+	}
+	else{
+	    return 0;
+	}
+
+	for(int loop = 0; loop < movesRow.length; loop++){
+	    countH(row + movesRow[loop], col + movesCol[loop], level);
+	    
+	}
+	board[row][col] = 0; //why doesn't moving this into the for loop work >:(
+
+	if(level - 1 == board.length * board[row].length){
+	    numSolutions++;
+	}
+
+	return numSolutions;
+    }
+    
     
     public static void main(String[] args){
 	System.out.print("\033[H\033[2J");  //resets cursor to default location
@@ -169,10 +159,12 @@ public class KnightBoard{
 
 	KnightBoard a = new KnightBoard(5, 5);
 
-	//System.out.println(a);
-	System.out.println(a.solve(2, 2));
+	System.out.println(a);
+	//System.out.println(a.solve(0,0));
+	System.out.println(a.countSolutions(0, 0));
 	System.out.println(a);
 	
 	
     }
 }
+
