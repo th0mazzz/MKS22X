950860ac8c101d7430199cbf269b9a1afbcae4c0
Thomas <tzhao2@stuy.edu>
2018-02-28 02:44:37 -0500

    Trying to write read file functionality

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -2,25 +2,41 @@ import java.util.*;
 import java.io.*;
 public class Maze{
 
-    private char[][] maze;
+
+    private char[][]maze;
     private boolean animate;//false by default
- 
+
     /*Constructor loads a maze text file, and sets animate to false by default.
 
       1. The file contains a rectangular ascii maze, made with the following 4 characters:
       '#' - Walls - locations that cannot be moved onto
       ' ' - Empty Space - locations that can be moved onto
       'E' - the location of the goal (exactly 1 per file)
+
       'S' - the location of the start(exactly 1 per file)
 
       2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
 
+
       3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then: print a meaningful error and exit the program.
+
     */
+
     public Maze(String filename){
-        //COMPLETE CONSTRUCTOR
+        try{
+	    File storageFile = new File(filename);
+	    Scanner in = new Scanner(storageFile);
+
+	    while(in.hasNext()){
+		String word = "'" + in.nextInt() + "'";
+		System.out.println(word);
+	    }
+	}catch(FileNotFoundException e){
+	    System.out.println("Caught");
+	}
     }
     
+
     private void wait(int millis){
          try {
              Thread.sleep(millis);
@@ -29,27 +45,43 @@ public class Maze{
          }
      }
 
+
     public void setAnimate(boolean b){
+
         animate = b;
+
     }
 
+
     public void clearTerminal(){
+
         //erase terminal, go to top left of screen.
+
         System.out.println("\033[2J\033[1;1H");
+
     }
+
 
 
     /*Wrapper Solve Function returns the helper function
+
       Note the helper function has the same name, but different parameters.
       Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
+
     */
     public int solve(){
+
             //find the location of the S. 
+
 
             //erase the S
 
+
             //and start solving at the location of the s.
+
             //return solve(???,???);
+
+	return -1;//so it compiles
     }
 
     /*
@@ -60,27 +92,34 @@ public class Maze{
       Returns the number of @ symbols from S to E when the maze is solved,
       Returns -1 when the maze has no solution.
 
+
       Postcondition:
+
         The S is replaced with '@' but the 'E' is not.
+
         All visited spots that were not part of the solution are changed to '.'
+
             Note: This is not required based on the algorithm, it is just nice visually to see.
         All visited spots that are part of the solution are changed to '@'
     */
     private int solve(int row, int col){ //you can add more parameters since this is private
 
+
         //automatic animation! You are welcome.
         if(animate){
+
             clearTerminal();
             System.out.println(this);
-            wait(20);
-public static void main(String[]args){
-        Maze f;
-        f = new Maze("data1.dat");//true animates the maze.
-        
-        f.setAnimate(true);
-        f.solve();
 
-        System.out.println(f);
+            wait(20);
+        }
+
+        //COMPLETE SOLVE
+
+        return -1; //so it compiles
     }
 
+
 }
+
+
