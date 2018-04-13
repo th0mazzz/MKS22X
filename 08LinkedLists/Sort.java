9d307c5832ca7ce88faa60d8db56531ec4edf53d
Thomas <tzhao2@stuy.edu>
2018-04-13 14:49:13 -0400

    Wrote a log10 function and still working on sorts

--- 08LinkedLists/Sort.java
+++ 08LinkedLists/Sort.java
@@ -1,16 +1,28 @@
 public class Sort{
     public static void radixsort(MyLinkedListImproved<Integer> data){
 	
-	
-	
+	Integer max = findMaxDigits(data);
+	int maxDigits = log10(max);
+	System.out.println(maxDigits);
+
+	MyLinkedListImproved<Integer>[] buckets = new MyLinkedListImproved[10];
 	
 	
     }
 
-    public static void findMaxDigits(MyLinkedListImproved<Integer> data){
+    public static Integer findMaxDigits(MyLinkedListImproved<Integer> data){
 	int maxIndex = data.max();
 	Integer maxValue = data.get(maxIndex);
-	System.out.println(maxValue);
+	return maxValue;
+    }
+
+    public static int log10(Integer num){
+	int tens = 0;
+	while(num >= 10){
+	    tens++;
+	    num = num / 10;
+	}
+	return tens + 1;
     }
 
     public static void main(String[] args){
@@ -18,6 +30,9 @@ public class Sort{
 	a.add(new Integer (23));
 	a.add(new Integer (44));
 	a.add(new Integer (12));
-	findMaxDigits(a);
+	a.add(new Integer (98));
+	a.add(new Integer (987654321));
+	radixsort(a);
+
     }
 }    
