e374dd6ff1f2a63b232ef95013a35df5ce82d6b3
Thomas <tzhao2@stuy.edu>
2018-04-17 23:27:32 -0400

    Eval should now fully work

--- 09Stack/Evaluate.java
+++ 09Stack/Evaluate.java
@@ -15,28 +15,63 @@ public class Evaluate{
 	for(int index = 0; index < express.length(); index++){
 	    String token = "";
 	    while(index < express.length() && express.charAt(index) != ' '){
-		System.out.println(index);
+		
 		token = token + express.charAt(index);
 		index++;
 		
 	    }
-	    System.out.println("token: " + token);
 	    
 	    tokens[arrayIndex] = token;
 	    arrayIndex++;
 	}
 
+	Stack<Double> stackie = new Stack<>();
+	double answer = 0.0;
+	
 	for(int index = 0; index < tokens.length; index++){
-	    System.out.print(tokens[index] + ", ");
+	    if(!((tokens[index].equals("+") ||
+		  (tokens[index].equals("-")) ||
+		  (tokens[index].equals("*")) ||
+		  (tokens[index].equals("/")) ||
+		  (tokens[index].equals("%"))))){
+
+		stackie.push(Double.parseDouble(tokens[index]));
+		
+	    }else{
+
+		if(tokens[index].equals("+")){
+		    stackie.push(stackie.pop() + stackie.pop());
+		}
+		if(tokens[index].equals("-")){
+		    double second = stackie.pop();
+		    stackie.push(stackie.pop() - second);
+		}
+		if(tokens[index].equals("*")){
+		    stackie.push(stackie.pop() * stackie.pop());
+		}
+		if(tokens[index].equals("/")){
+		    double second = stackie.pop();
+		    stackie.push(stackie.pop() / second);
+		}
+		if(tokens[index].equals("%")){
+		    double second = stackie.pop();
+		    stackie.push(stackie.pop() % second);
+		}
+		
+	    }
 	}
 
-	return 0.0;
+	
+	return stackie.pop();
     }
     
     public static void main(String[] args){
 
 	String a = "8 2 + 99 9 - * 2 + 9 -";
-	System.out.println("\n\n\n" + eval(a));
+	System.out.println("\n\n\n" + eval(a)); //893.0
+
+	System.out.println(eval("10 2.0 +")); //30.0
+	System.out.println(eval("11 3 - 4 + 2.5 *")); //12.0
 	
     }
     
