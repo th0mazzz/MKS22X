a04cbea583b5f018653b0d26b6f5c9734fc13269
Thomas <tzhao2@stuy.edu>
2018-04-28 15:24:26 -0400

    Skeleton for the MyHeap

---
+++ 12MyHeap/MyHeap.java
@@ -0,0 +1,29 @@
+public class MyHeap{
+
+    int[] array;
+    int size;
+    
+    public MyHeap(){
+       
+    }
+
+    public MyHeap(boolean isMax){
+	
+    }
+
+    public void add(String s){
+
+    }
+
+    public String remove(){
+	return "placeholder";
+    }
+    
+    public String peek(){
+	return "placeholder";
+    }
+
+    public int size(){
+	return -1;
+    }
+}
