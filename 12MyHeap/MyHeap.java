0a22037e91a1f43681127f5f60950e56cf2cedbd
Thomas <tzhao2@stuy.edu>
2018-04-29 21:53:42 -0400

    add and remove are done

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyHeap{
 
     public String[] array;
@@ -5,19 +6,19 @@ public class MyHeap{
     public boolean isMax;
     
     public MyHeap(){
-	array = new String[17];
+	array = new String[10];
 	size = 0;
 	isMax = true;
     }
 
     public MyHeap(boolean isMax){
-	array = new String[17];
+	array = new String[10];
 	size = 0;
 	this.isMax = isMax;
     }
 
     public void resize(){
-	String[] larger = new String[(array.length - 1) * 2 + 1];
+	String[] larger = new String[array.length * 2];
 	for(int i = 0; i < array.length; i++){
 	    larger[i] = array[i];
 	}
@@ -25,11 +26,83 @@ public class MyHeap{
     }
 
     public void add(String s){
-	
+	if(size() == array.length){
+	    resize();
+	}
+	array[size()] = s;
+	int current = size();
+	int parent = (size() - 1) / 2;
+	if(isMax){
+	    while(array[current].compareTo(array[parent]) > 0){
+		String temp = array[current];
+		array[current] = array[parent];
+		array[parent] = temp;
+		current = parent;
+		parent = (parent - 1) / 2;
+	    }
+	}else{
+	    while(array[current].compareTo(array[parent]) < 0){
+		String temp = array[current];
+		array[current] = array[parent];
+		array[parent] = temp;
+		current = parent;
+		parent = (parent - 1) / 2;
+	    }
+	}
+	size++;
     }
 
     public String remove(){
-	return "placeholder";
+	if(size() == 0){
+	    throw new NoSuchElementException();
+	}
+	String oldRoot = array[0];
+	array[0] = array[size() - 1];
+	array[size() - 1] = null;
+	size--;
+	int current = 0;
+	int left = current * 2 + 1;
+	int right = current * 2 + 2;
+	if(isMax){
+	    while(left < size() && array[current].compareTo(array[left]) < 0 &&
+	    array[current].compareTo(array[right]) < 0){
+		if(array[left].compareTo(array[right]) > 0){
+		    String temp = array[left];
+		    array[left] = array[current];
+		    array[current] = temp;
+		    current = left;
+		    left = current * 2 + 1;
+		    right = current * 2 + 2;
+		}else{
+		    String temp = array[right];
+		    array[right] = array[current];
+		    array[current] = temp;
+		    current = right;
+		    left = current * 2 + 1;
+		    right = current * 2 + 2;
+		}
+	    }
+	}else{
+	    while(left < size() && array[current].compareTo(array[left]) > 0 &&
+		  array[current].compareTo(array[right]) > 0){
+		if(array[left].compareTo(array[right]) < 0){
+		    String temp = array[left];
+		    array[left] = array[current];
+		    array[current] = temp;
+		    current = left;
+		    left = current * 2 + 1;
+		    right = current * 2 + 2;
+		}else{
+		    String temp = array[right];
+		    array[right] = array[current];
+		    array[current] = temp;
+		    current = right;
+		    left = current * 2 + 1;
+		    right = current * 2 + 2;
+		}
+	    }
+	}
+	return oldRoot;
     }
     
     public String peek(){
@@ -50,9 +123,21 @@ public class MyHeap{
     }
 
     public static void main(String[] args){
-	MyHeap a = new MyHeap();
+	MyHeap a = new MyHeap(false);
 	System.out.println(a);
-	a.resize();
+	//a.resize();
+	a.add("b");
+	a.add("b");
+	a.add("c");
+	a.add("d");
+	a.add("e");
+	a.add("f");
+	a.add("g");
+	a.add("h");
+	a.add("n");
+	a.add("a");
+	System.out.println(a);
+	System.out.println(a.remove());
 	System.out.println(a);
     }
 }
