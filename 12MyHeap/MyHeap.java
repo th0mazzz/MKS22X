172b4da3b10aff42fc19bcd28ddf6a98a66f9b57
Thomas <tzhao2@stuy.edu>
2018-04-29 20:26:13 -0400

    Finished constructors, resize, size, toString

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -1,18 +1,31 @@
 public class MyHeap{
 
-    int[] array;
-    int size;
+    public String[] array;
+    public int size;
+    public boolean isMax;
     
     public MyHeap(){
-       
+	array = new String[17];
+	size = 0;
+	isMax = true;
     }
 
     public MyHeap(boolean isMax){
-	
+	array = new String[17];
+	size = 0;
+	this.isMax = isMax;
+    }
+
+    public void resize(){
+	String[] larger = new String[(array.length - 1) * 2 + 1];
+	for(int i = 0; i < array.length; i++){
+	    larger[i] = array[i];
+	}
+	array = larger;
     }
 
     public void add(String s){
-
+	
     }
 
     public String remove(){
@@ -24,6 +37,22 @@ public class MyHeap{
     }
 
     public int size(){
-	return -1;
+	return size;
+    }
+
+    public String toString(){
+	String returnString = "[";
+	for(int index = 0; index < array.length; index++){
+	    returnString = returnString +  array[index] + ", ";
+	}
+	returnString = returnString.substring(0, returnString.length() - 2);
+	return returnString + "]";
+    }
+
+    public static void main(String[] args){
+	MyHeap a = new MyHeap();
+	System.out.println(a);
+	a.resize();
+	System.out.println(a);
     }
 }
