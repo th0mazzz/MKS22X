e68e6c5e4601051ca3fef9dd0c108d6a0d5f970d
Thomas <tzhao2@stuy.edu>
2018-05-07 10:25:43 -0400

    Created the base files for this lab

---
+++ 14FrontierMazeSolver/Location.java
@@ -0,0 +1,13 @@
+public class Location{
+
+    private int x, y;
+    private Location previous;
+
+    public Location (int _x, int _y, Location prev){
+	
+    }
+
+    public int getX(){return x;}
+    public int getY(){return y;}
+    public Location getPrev(){return previous;}
+}
