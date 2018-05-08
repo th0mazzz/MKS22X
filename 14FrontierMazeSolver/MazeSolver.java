345c31784a9d508a21bf6e161fcfd4d4adea71f5
Thomas <tzhao2@stuy.edu>
2018-05-08 19:16:43 -0400

    So it kind of works? not really? meh.

--- 14FrontierMazeSolver/MazeSolver.java
+++ 14FrontierMazeSolver/MazeSolver.java
@@ -15,8 +15,18 @@ public class MazeSolver{
 
     /*
 
-      
-
+      adds start to frontier
+      while frontier has next
+      if element is the end {
+           while getPrev is not null
+	   set all getPrev values to '@'
+       return true
+      }else{
+          remove first element in frontier, add its neighbors to the back
+	  set the location to a '.'
+      }
+      if all else fails return false :D
+	 
      */
     public boolean solve(int mode){
 
@@ -33,9 +43,32 @@ public class MazeSolver{
 	frontier.add(start);
 
 	while(frontier.hasNext()){
+	    
+	    Location next = frontier.next();
 
+	    if(maze.get(next.getX(), next.getY()) == 'E'){
+
+		Location prev = next;
+		while(prev.getPrev() != null){
+		    maze.set(prev.getX(), prev.getY(), '@');
+		    System.out.println(maze); //print statement
+		}
+
+		return true;
+		
+	    }else{
+		
+		maze.set(next.getX(), next.getY(), '.');
+
+		Location[] neighs = maze.getNeighbors(next);
+		for(int i = 0; i < neighs.length; i++){
+		    frontier.add(neighs[i]);
+		}
+
+		System.out.println(maze); //another one
+		
+	    }
 	}
-	
 	
 	return false;
     }
